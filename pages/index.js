82bfb26e31d5da9401dbaf83c85e6f07d4299427
emredalkiran <emre59dalkiran@gmail.com>
2021-01-20 13:32:42 +0300

    Homepage to show posts statically

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Link from 'next/link'
+import Post from './components/post'
 export default function Home() {
+
+  const blogContent = {
+    title: 'How this blog is built',
+    description: 'First part of what this blog is about and how it is built.'
+  }
+
   return (
     <div className="container">
       <Head>
@@ -14,48 +21,47 @@ export default function Home() {
 
       <main className="main">
         <h1 className="title">
-          Hi, I'm Emre
-        </h1>
-
+          Hi, I&apos;m Emre
+        </h1>     
         <p className="description">
           A software engineer who loves to build things
         </p>
         <div className="card">
-        <Image
-          src="/blog-profile.jpg"
-          alt="Picture of the author"
-          width={250}
-          height={250}
-          className="avatar"
-      />
-        <div className="grid">
-        <a href="https://github.com/emredalkiran" target="_blank" className="link-logo">
-          <img src="/github.svg" alt="Github Logo" className="logo"/>
-        </a>
-        <a href="https://www.linkedin.com/in/emre-dalkiran-140787bb" target="_blank" className="link-logo">
-          <img src="/linkedin.svg" alt="Linkedin Logo" className="logo"/>
-        </a>
-        <a href="https://twitter.com/seldomseenkdg" target="_blank" className="link-logo">
-          <img src="/twitter.svg" alt="Twitter Logo" className="logo"/>
-        </a>
-        <a href="mailto:emre59dalkiran@gmail.com" className="link-logo">
-          <img src="/email.svg" alt="Email Logo" className="logo"/>
-        </a>
+          <Image
+            src="/blog-profile.jpg"
+            alt="Picture of the author"
+            width={250}
+            height={250}
+            className="avatar"
+          />
+          <div className="grid">
+            <a href="https://github.com/emredalkiran" rel='noreferrer' target="_blank" className="link-logo">
+              <img src="/github.svg" alt="Github Logo" className="logo"/>
+            </a>
+            <a href="https://www.linkedin.com/in/emre-dalkiran-140787bb" target="_blank" rel='noreferrer' className="link-logo">
+              <img src="/linkedin.svg" alt="Linkedin Logo" className="logo"/>
+            </a>
+            <a href="https://twitter.com/seldomseenkdg" target="_blank" rel='noreferrer' className="link-logo">
+              <img src="/twitter.svg" alt="Twitter Logo" className="logo"/>
+            </a>
+            <a href="mailto:emre59dalkiran@gmail.com" className="link-logo">
+              <img src="/email.svg" alt="Email Logo" className="logo"/>
+            </a>
+          </div>
         </div>
-        </div>
-        <p className="description">This blog is intended to be a personal space where I would like to write about code, music and some other cool stuff</p>
-      </main>
+        <section className="section">
+          <p className="description">This blog is intended to be a personal space where I would like to write about code, music and some other cool stuff.</p>
+        </section>
+        <section className="section">
+          <h2 className="title ">Latest Posts</h2>
+          <div className="grid">
+            <Link href="/how-this-blog-is-built-1" passHref>
+              <Post content={ blogContent }></Post>
+            </Link>
+          </div>
+        </section>
 
-      <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo"/>
-        </a>
-      </footer>
+      </main>
     </div>
   )
 }
